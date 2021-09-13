eb31a9a6a8632c80ea374b21298eac1cec632dad
therman mcmillan <mcmillanj0106@yahoo.com>
2021-09-13 14:03:19 -0400

    4 done

--- index.js
+++ index.js
@@ -86,8 +86,19 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inningcb,numOfInn){
+  let homeTeam = 0;
+  let awayTeam = 0;
+for(let i = 0; i < numOfInn;i++ )
+{
+  homeTeam = homeTeam + inning();
+  awayTeam = awayTeam + inning();
+
+}  
+return {
+  Home: homeTeam, 
+  Away: awayTeam,
+}    
 }
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
@@ -95,10 +106,13 @@ Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningcb) {
+  return {
+    Home:inning(),
+    Away:inning(),
+  }
 }
-
+getInningScore(inning);
 
 /* ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
